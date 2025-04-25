daa079b5c59bcc4a0e3bd78fa9867c927b42a9ad
x12fr <richrobux45@gmail.com>
2025-04-24 22:02:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,79 +1,40 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const path = require('path');
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
 const users = {};
-const timeouts = {};
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
-
-  socket.on('register', ({ username, profilePic }) => {
-    users[socket.id] = { username, profilePic };
-    io.emit('updateUsers', users);
+io.on('connection', socket => {
+  socket.on('register', ({ user, pass }) => {
+    if (users[user]) return socket.emit('loginError', 'Username taken');
+    users[user] = { pass, profilePic: `https://robohash.org/${user}` };
+    socket.emit('loginSuccess', { username: user, profilePic: users[user].profilePic });
   });
 
-  socket.on('chatMessage', (msg) => {
-    const user = users[socket.id];
-    if (user && !timeouts[socket.id]) {
-      io.emit('chatMessage', {
-        username: user.username,
-        profilePic: user.profilePic,
-        text: msg
-      });
+  socket.on('login', ({ user, pass }) => {
+    if (user === 'X12' && pass === '331256444') {
+      return socket.emit('loginSuccess', { username: user, profilePic: `https://robohash.org/${user}` });
     }
-  });
-
-  socket.on('image', (data) => {
-    const user = users[socket.id];
-    if (user && !timeouts[socket.id]) {
-      io.emit('image', {
-        username: user.username,
-        profilePic: user.profilePic,
-        image: data
-      });
+    if (!users[user] || users[user].pass !== pass) {
+      return socket.emit('loginError', 'Invalid credentials');
     }
+    socket.emit('loginSuccess', { username: user, profilePic: users[user].profilePic });
   });
 
-  socket.on('admin:strobe', (duration) => {
-    io.emit('strobe', duration);
-  });
-
-  socket.on('admin:timeout', ({ targetUsername, duration }) => {
-    for (const [id, user] of Object.entries(users)) {
-      if (user.username === targetUsername) {
-        timeouts[id] = true;
-        io.to(id).emit('timeout', duration);
-        setTimeout(() => {
-          delete timeouts[id];
-        }, duration * 1000);
-      }
-    }
-  });
-
-  socket.on('admin:playAudio', (url) => {
-    io.emit('playAudio', url);
-  });
-
-  socket.on('disconnect', () => {
-    delete users[socket.id];
-    delete timeouts[socket.id];
-    io.emit('updateUsers', users);
-  });
+  socket.on('message', data => io.emit('message', data));
+  socket.on('image', data => io.emit('image', data));
+  socket.on('adminStrobe', duration => io.emit('strobe', duration));
+  socket.on('adminAudio', url => io.emit('playAudio', url));
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+http.listen(process.env.PORT || 3000, () => {
+  console.log('Sharcord running');
 });
