ff610da5e6c69e15b8a8354dc6ee78d789fa5272
x12fr <richrobux45@gmail.com>
2025-04-25 11:25:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,128 +1,55 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-const bodyParser = require('body-parser');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
 
-// Static files
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-// Fake Database
-const usersDB = {
-  'X12': '331256444' // Owner account
-};
-const admins = new Set(['X12']);
+const users = {};
+const admins = ['X12']; // Add more usernames to this array for multiple admins
+const owner = 'X12';
 
-// Serve Pages
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
-app.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'register.html'));
-});
-app.get('/chat', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'chat.html'));
-});
+io.on('connection', socket => {
+  let user;
 
-// Handle Login
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  if (usersDB[username] && usersDB[username] === password) {
-    return res.redirect(`/chat?user=${username}`);
-  } else {
-    return res.send('Invalid username or password. <a href="/login">Try again</a>.');
-  }
-});
-
-// Handle Register
-app.post('/register', (req, res) => {
-  const { username, password } = req.body;
-  if (usersDB[username]) {
-    return res.send('Username already exists. <a href="/register">Try again</a>.');
-  }
-  usersDB[username] = password;
-  return res.redirect(`/chat?user=${username}`);
-});
-
-// WebSocket Stuff
-const users = {}; 
-const userSockets = {};
-
-io.on('connection', (socket) => {
-  let currentUser = '';
-
-  socket.on('check admin', ({ username }, cb) => {
-    cb({ isAdmin: admins.has(username) });
+  socket.on('chat message', data => {
+    if (!users[data.username]) return;
+    io.emit('chat message', data);
   });
 
-  socket.on('chat message', ({ user, message }) => {
-    currentUser = user;
-    users[socket.id] = user;
-    userSockets[user] = socket.id;
-    io.emit('chat message', { user, message });
+  socket.on('file upload', data => {
+    io.emit('file upload', data);
   });
 
-  socket.on('private message', ({ to, from, message }) => {
-    const targetSocketId = userSockets[to];
-    if (targetSocketId) {
-      io.to(targetSocketId).emit('private message', { from, message });
+  socket.on('check admin', ({ username }, callback) => {
+    user = username;
+    users[username] = socket.id;
+    callback(admins.includes(username));
+  });
+
+  socket.on('announcement', ({ username, announcement }) => {
+    if (admins.includes(username)) {
+      io.emit('announcement', { username, announcement });
     }
   });
 
-  socket.on('announcement', (msg) => {
-    io.emit('announcement', msg);
+  socket.on('kick user', ({ username }) => {
+    const id = users[username];
+    if (id) io.to(id).emit('kick');
   });
 
-  socket.on('flash', (targetUsername) => {
-    const targetSocket = userSockets[targetUsername];
-    if (targetSocket) {
-      io.to(targetSocket).emit('flash');
-    }
-  });
-
-  socket.on('jumpscare', () => {
-    for (let id in users) {
-      io.to(id).emit('jumpscare');
-    }
-  });
-
-  socket.on('redirect', ({ user, url }) => {
-    const socketId = userSockets[user];
-    if (socketId) {
-      io.to(socketId).emit('redirect', url);
-    }
-  });
-
-  socket.on('timeout', ({ user, seconds }) => {
-    const socketId = userSockets[user];
-    if (socketId) {
-      io.to(socketId).emit('timeout', seconds);
-    }
-  });
-
-  socket.on('kick', (user) => {
-    const socketId = userSockets[user];
-    if (socketId) {
-      io.to(socketId).emit('kick');
-      io.sockets.sockets.get(socketId)?.disconnect();
+  socket.on('jumpscare everyone', () => {
+    if (user === owner) {
+      io.emit('jumpscare');
     }
   });
 
   socket.on('disconnect', () => {
-    const user = users[socket.id];
-    delete userSockets[user];
-    delete users[socket.id];
+    delete users[user];
   });
 });
 
-server.listen(3000, () => {
-  console.log('Sharcord server running on http://localhost:3000');
+http.listen(3000, () => {
+  console.log('Sharcord server running on port 3000');
 });
