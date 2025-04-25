80aaaec3bd3bcda709db8fc30322f1bd8160e297
x12fr <richrobux45@gmail.com>
2025-04-24 20:33:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-
-const PORT = process.env.PORT || 3000;
-
+const server = http.createServer(app);
+const io = new Server(server);
 app.use(express.static("public"));
+app.use(express.json());
 
-io.on("connection", socket => {
-  socket.on("chat message", ({ user, message }) => {
-    io.emit("chat message", { user, message });
+const users = {};
+const passwords = {};
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  if (users[username]) return res.json({ success: false, message: "Username already taken" });
+  users[username] = username;
+  passwords[username] = password;
+  res.json({ success: true });
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (passwords[username] === password) {
+    res.json({ success: true, admin: username === "X12" && password === "331256444" });
+  } else {
+    res.json({ success: false });
+  }
+});
+
+io.on("connection", (socket) => {
+  let user = "Anonymous";
+  socket.on("join", (username) => {
+    user = username;
+    io.emit("message", `${user} joined the chat`);
+  });
+  socket.on("message", (msg) => {
+    io.emit("message", `${user}: ${msg}`);
   });
 });
 
-http.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(3000, () => {
+  console.log("Sharcord running on http://localhost:3000");
 });
