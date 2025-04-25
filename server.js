02d671a49cdaefa652e48acdba7ef737a8c4c4d1
x12fr <richrobux45@gmail.com>
2025-04-24 22:41:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,84 +1,88 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const path = require('path');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const server = http.createServer(app);
+const io = new Server(server);
+const path = require("path");
 
-const users = {};
-const timeouts = {};
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+let users = {};
+let timeouts = {};
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.use(express.static("public")); // assumes your client files are in /public
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.post('/register', (req, res) => {
-  const { username, password } = req.body;
-  if (users[username]) return res.status(400).send('Username taken');
-  users[username] = { password, profilePic: null };
-  res.status(200).send('Registered');
-});
+io.on("connection", (socket) => {
+  console.log("User connected");
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  const user = users[username];
-  if (!user || user.password !== password) return res.status(400).send('Invalid');
-  res.status(200).send('Success');
-});
+  socket.on("login", ({ username, password }) => {
+    users[socket.id] = { username, isAdmin: username === "X12", timeout: false };
+    socket.emit("loginSuccess", users[socket.id]);
+    io.emit("chat", { user: "System", msg: `${username} joined.` });
+  });
 
-io.on('connection', (socket) => {
-  socket.on('join', ({ username, profilePic }) => {
-    socket.username = username;
-    socket.profilePic = profilePic;
-    socket.broadcast.emit('user joined', { username });
-
-    if (timeouts[username]) {
-      socket.emit('timeout', timeouts[username]);
+  socket.on("chat", (msg) => {
+    const user = users[socket.id];
+    if (user && !user.timeout) {
+      io.emit("chat", { user: user.username, msg, pic: user.pic });
     }
   });
 
-  socket.on('chat message', (msg) => {
-    if (!timeouts[socket.username]) {
-      io.emit('chat message', {
-        username: socket.username,
-        text: msg,
-        profilePic: socket.profilePic
-      });
+  socket.on("register", ({ username, password, pic }) => {
+    users[socket.id] = { username, isAdmin: username === "X12", pic, timeout: false };
+    socket.emit("loginSuccess", users[socket.id]);
+    io.emit("chat", { user: "System", msg: `${username} joined.` });
+  });
+
+  socket.on("sendImage", (url) => {
+    const user = users[socket.id];
+    if (user && !user.timeout) {
+      io.emit("image", { user: user.username, url, pic: user.pic });
     }
   });
 
-  socket.on('image', (dataUrl) => {
-    io.emit('image', {
-      username: socket.username,
-      dataUrl,
-      profilePic: socket.profilePic
-    });
+  socket.on("flash", (duration) => {
+    const user = users[socket.id];
+    if (user?.isAdmin) {
+      io.emit("flash", duration);
+    }
   });
 
-  socket.on('strobe', (duration) => {
-    io.emit('strobe', duration);
+  socket.on("timeout", ({ userToTimeout, mins }) => {
+    const userId = Object.keys(users).find(id => users[id].username === userToTimeout);
+    if (userId) {
+      users[userId].timeout = true;
+      timeouts[userToTimeout] = Date.now() + mins * 60000;
+      io.emit("timeoutInfo", { user: userToTimeout, mins });
+    }
   });
 
-  socket.on('play audio', (url) => {
-    io.emit('play audio', url);
+  socket.on("playAudio", (url) => {
+    const user = users[socket.id];
+    if (user?.isAdmin) {
+      io.emit("playAudio", url);
+    }
   });
 
-  socket.on('timeout user', ({ username, duration }) => {
-    timeouts[username] = duration;
-    io.emit('timeout', { username, duration });
+  socket.on("redirectUser", ({ userToRedirect, link }) => {
+    const userId = Object.keys(users).find(id => users[id].username === userToRedirect);
+    if (userId) {
+      io.to(userId).emit("redirect", link);
+    }
   });
 
-  socket.on('redirect user', ({ username, url }) => {
-    io.emit('redirect user', { username, url });
+  socket.on("disconnect", () => {
+    const user = users[socket.id];
+    if (user) {
+      io.emit("chat", { user: "System", msg: `${user.username} left.` });
+      delete users[socket.id];
+    }
   });
 });
 
-http.listen(3000, () => {
-  console.log('Sharcord server running on port 3000');
-});
+server.listen(PORT, () => console.log("Sharcord server live on port", PORT));
