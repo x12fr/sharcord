b8e7dbe35fa06be0974641a81eab1a3c31be89e0
x12fr <richrobux45@gmail.com>
2025-04-26 14:14:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,83 +5,55 @@ const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const users = {}; // username -> password
-const profilePictures = {}; // username -> profile pic url
+const users = {};
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/login.html');
 });
 
-app.get('/register', (req, res) => {
-  res.sendFile(__dirname + '/public/register.html');
+app.get('/login.html', (req, res) => {
+    res.sendFile(__dirname + '/public/login.html');
 });
 
-app.post('/register', (req, res) => {
-  const { username, password } = req.body;
-  if (users[username]) {
-    return res.send('Username already taken.');
-  }
-  users[username] = password;
-  profilePictures[username] = '/defaultpfp.png'; // set a default profile picture
-  return res.redirect('/login');
-});
-
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/public/login.html');
-});
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  if (users[username] && users[username] === password) {
-    return res.redirect(`/chat.html?username=${encodeURIComponent(username)}`);
-  }
-  return res.send('Invalid login.');
+app.get('/register.html', (req, res) => {
+    res.sendFile(__dirname + '/public/register.html');
 });
 
 app.get('/chat.html', (req, res) => {
-  res.sendFile(__dirname + '/public/chat.html');
+    res.sendFile(__dirname + '/public/chat.html');
+});
+
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    if (users[username]) {
+        return res.send('Username taken.');
+    }
+    users[username] = { password };
+    return res.redirect('/login.html');
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (!users[username] || users[username].password !== password) {
+        return res.send('Invalid login.');
+    }
+    return res.redirect('/chat.html');
 });
 
 io.on('connection', (socket) => {
-  let user = null;
+    socket.on('join', ({ username, profilePic }) => {
+        socket.username = username;
+        socket.profilePic = profilePic;
+    });
 
-  socket.on('set username', (username) => {
-    user = username;
-    console.log(`${user} connected`);
-  });
-
-  socket.on('chat message', (msg) => {
-    if (user) {
-      io.emit('chat message', { username: user, message: msg });
-    }
-  });
-
-  // admin commands
-  socket.on('announce', (text) => {
-    io.emit('announcement', text);
-  });
-
-  socket.on('strobe', (duration) => {
-    io.emit('strobe', duration);
-  });
-
-  socket.on('timeout', (data) => {
-    io.emit('timeout', data.duration);
-  });
-
-  socket.on('redirect', (data) => {
-    io.emit('redirect', data.link);
-  });
-
-  socket.on('jumpscare', (data) => {
-    io.emit('jumpscare', data);
-  });
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data);
+    });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+http.listen(3000, () => {
+    console.log('Sharcord live on port 3000');
 });
