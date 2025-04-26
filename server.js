1aa43df4fe582e44cd784fe6b44b43896d2e2666
x12fr <richrobux45@gmail.com>
2025-04-25 21:03:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,103 +1,86 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-const path = require("path");
-const fs = require("fs");
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+// Serve static files from public folder
+app.use(express.static('public'));
 
-app.use(express.static("public"));
-app.use(express.json());
+// Pages
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
+app.get('/chat', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'chat.html'));
+});
+
+// Data
 const users = {};
-const admins = ["X12"];
+const sockets = {};
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('register', ({ username, password }) => {
+        if (users[username]) {
+            socket.emit('registerFailed', 'Username already taken.');
+        } else {
+            users[username] = { password };
+            socket.emit('registerSuccess');
+        }
+    });
+
+    socket.on('login', ({ username, password }) => {
+        if (users[username] && users[username].password === password) {
+            sockets[socket.id] = username;
+            socket.emit('loginSuccess', { username });
+        } else {
+            socket.emit('loginFailed', 'Invalid credentials.');
+        }
+    });
+
+    socket.on('sendMessage', (data) => {
+        io.emit('chatMessage', data);
+    });
+
+    socket.on('adminFlash', ({ duration }) => {
+        io.emit('flashScreen', { duration });
+    });
+
+    socket.on('adminAnnouncement', ({ message }) => {
+        io.emit('announcement', { message });
+    });
+
+    socket.on('adminJumpScare', ({ imageUrl, audioUrl }) => {
+        io.emit('jumpScare', { imageUrl, audioUrl });
+    });
+
+    socket.on('adminSpamTabs', ({ username, link, amount }) => {
+        for (let id in sockets) {
+            if (sockets[id] === username) {
+                io.to(id).emit('spamTabs', { link, amount });
+            }
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+        delete sockets[socket.id];
+    });
 });
 
-app.post("/register", (req, res) => {
-  const { username, password } = req.body;
-  if (users[username]) {
-    return res.status(400).json({ error: "Username taken" });
-  }
-  users[username] = { password, isAdmin: admins.includes(username) };
-  res.json({ success: true });
-});
-
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  if (!users[username] || users[username].password !== password) {
-    return res.status(401).json({ error: "Invalid credentials" });
-  }
-  res.json({ success: true, isAdmin: users[username].isAdmin });
-});
-
-io.on("connection", (socket) => {
-  console.log("A user connected");
-
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-  });
-
-  socket.on("admin action", (action) => {
-    switch (action.type) {
-      case "strobe":
-        io.emit("admin action", { type: "strobe", duration: action.duration });
-        break;
-      case "timeout":
-        io.emit("admin action", {
-          type: "timeout",
-          user: action.user,
-          duration: action.duration,
-        });
-        break;
-      case "kick":
-        io.emit("admin action", { type: "kick", user: action.user });
-        break;
-      case "redirect":
-        io.emit("admin action", {
-          type: "redirect",
-          user: action.user,
-          link: action.link,
-        });
-        break;
-      case "spam":
-        io.emit("admin action", {
-          type: "spam",
-          user: action.user,
-          count: action.count,
-        });
-        break;
-      case "announcement":
-        io.emit("admin action", { type: "announcement", text: action.text });
-        break;
-      case "grant_admin":
-        io.emit("admin action", {
-          type: "grant_admin",
-          user: action.user,
-          time: action.time,
-        });
-        break;
-      case "jumpscare":
-        io.emit("admin action", {
-          type: "jumpscare",
-          image: action.image,
-          audio: action.audio,
-        });
-        break;
-      case "clear":
-        io.emit("admin action", { type: "clear" });
-        break;
-    }
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
-
+// Start server
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
