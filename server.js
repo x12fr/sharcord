ae685ea1c1e28330311ab5d2decc7e2502c013e0
x12fr <richrobux45@gmail.com>
2025-04-26 13:35:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,69 +1,45 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const bodyParser = require('body-parser');
-const path = require('path');
 
-let users = {};
+app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname, '/')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => res.sendFile(__dirname + '/login.html'));
-app.get('/register.html', (req, res) => res.sendFile(__dirname + '/register.html'));
-app.get('/login.html', (req, res) => res.sendFile(__dirname + '/login.html'));
-app.get('/chat.html', (req, res) => res.sendFile(__dirname + '/chat.html'));
-app.get('/secret.html', (req, res) => res.sendFile(__dirname + '/secret.html'));
-
-app.post('/register', (req, res) => {
-  const { username, password } = req.body;
-  if (users[username]) {
-    return res.status(400).send('Username already taken');
-  }
-  users[username] = { password, profilePic: '/default.png' };
-  res.redirect('/login.html');
+// Serve the main pages
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  if (!users[username] || users[username].password !== password) {
-    return res.status(400).send('Invalid login');
-  }
-  res.redirect('/chat.html');
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-io.on('connection', socket => {
-  socket.on('chat message', data => {
-    io.emit('chat message', data);
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
+app.get('/chat', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'chat.html'));
+});
+
+// Set up basic Socket.io
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
 
-  socket.on('check admin', username => {
-    if (username === 'X12') {
-      socket.emit('enable admin');
-    }
-  });
+  // You can add admin events here later (strobe, timeout, jump scare, etc.)
 
-  socket.on('admin strobe', duration => {
-    io.emit('strobe', duration);
-  });
-
-  socket.on('admin timeout', data => {
-    io.emit('timeout', data.duration);
-  });
-
-  socket.on('admin redirect', user => {
-    io.emit('redirect');
-  });
-
-  socket.on('admin announcement', text => {
-    io.emit('announcement', text);
-  });
-
-  socket.on('admin jumpscare', data => {
-    io.emit('jumpscare', data);
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
   });
 });
 
-http.listen(3000, () => console.log('Sharcord running on port 3000'));
+// Start the server
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
