8e6f41872e9d4e698a7c46027e7f787031bf022d
x12fr <richrobux45@gmail.com>
2025-04-26 12:04:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,37 +2,104 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const users = {}; // username -> { password, pfp }
+const sockets = {}; // socket.id -> username
+let announcements = [];
+let redirects = {};
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-
-  socket.on('admin action', (action) => {
-    if (action.type === 'strobe') {
-      io.emit('strobe');
-    } else if (action.type === 'timeout') {
-      io.emit('timeout', action.user);
-    } else if (action.type === 'kick') {
-      io.emit('kick', action.user);
-    } else if (action.type === 'redirect') {
-      io.emit('redirect', action.user);
-    } else if (action.type === 'spamTabs') {
-      io.emit('spamTabs', action.user);
-    } else if (action.type === 'clearChat') {
-      io.emit('clearChat');
-    } else if (action.type === 'announcement') {
-      io.emit('admin action', { type: 'announcement', text: action.text });
-    } else if (action.type === 'secretRedirect') {
-      io.emit('admin action', { type: 'secretRedirect', user: action.user });
-    } else if (action.type === 'jumpscare') {
-      io.emit('jumpscare');
-    }
-  });
+// Serve the html pages properly
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/login.html'));
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/register.html'));
+});
+app.get('/chat', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/chat.html'));
+});
+app.get('/secret', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/secret.html'));
 });
 
-http.listen(3000, () => {
-  console.log('Sharcord running...');
+// Handle registration
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    if (users[username]) {
+        return res.send('Username already exists!');
+    }
+    users[username] = { password, pfp: 'default.png' };
+    res.redirect('/login');
+});
+
+// Handle login
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (!users[username] || users[username].password !== password) {
+        return res.send('Invalid login!');
+    }
+    res.redirect(`/chat?username=${username}`);
+});
+
+// Socket.io stuff
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('login', (username) => {
+        sockets[socket.id] = username;
+        socket.username = username;
+        io.emit('userlist', Object.values(sockets));
+    });
+
+    socket.on('chat message', (msg) => {
+        const username = sockets[socket.id];
+        const pfp = users[username]?.pfp || 'default.png';
+        io.emit('chat message', { username, pfp, msg });
+    });
+
+    socket.on('change pfp', (newpfp) => {
+        const username = sockets[socket.id];
+        if (users[username]) {
+            users[username].pfp = newpfp;
+        }
+    });
+
+    socket.on('announcement', (text) => {
+        announcements.push(text);
+        io.emit('announcement', text);
+        setTimeout(() => {
+            io.emit('clear announcement');
+        }, 10000);
+    });
+
+    socket.on('redirect user', (targetUser) => {
+        for (let id in sockets) {
+            if (sockets[id] === targetUser) {
+                io.to(id).emit('redirect secret');
+                setTimeout(() => {
+                    io.to(id).emit('redirect back');
+                }, 5000);
+            }
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+        delete sockets[socket.id];
+        io.emit('userlist', Object.values(sockets));
+    });
+});
+
+// Start server
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
