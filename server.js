ca31b1c6472006647c01f5911d2eb7097c2d3815
x12fr <richrobux45@gmail.com>
2025-04-26 16:14:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,91 +4,112 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
-
-// Data storage
-let servers = []; // [{ name: "Server1", channels: [{ name: "general", messages: [] }] }]
-
-// Serve static files
+// Serve public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-// --- Socket.io Logic ---
-io.on('connection', (socket) => {
-  console.log('A user connected.');
+// Server and channel structure
+let servers = [
+  { name: 'Main Server', channels: [{ name: 'general' }, { name: 'memes' }] }
+];
 
-  // Request for all servers
-  socket.on('requestServers', () => {
-    socket.emit('serverList', servers);
+let users = {}; // { socketId: { username, profilePic } }
+let timeouts = {}; // { username: timeoutEndTimestamp }
+
+// Socket.io
+io.on('connection', socket => {
+  console.log('User connected:', socket.id);
+
+  socket.on('user_connected', ({ username, profilePic }) => {
+    users[socket.id] = { username, profilePic };
+    io.to(socket.id).emit('server_list', servers);
   });
 
-  // Create server
-  socket.on('createServer', (serverName) => {
-    if (!servers.find(s => s.name === serverName)) {
-      servers.push({ name: serverName, channels: [] });
-      io.emit('serverList', servers);
-      console.log(`Server created: ${serverName}`);
+  socket.on('create_server', serverName => {
+    if (serverName.length > 0) {
+      servers.push({ name: serverName, channels: [{ name: 'general' }] });
+      io.emit('server_list', servers);
     }
   });
 
-  // Join server
-  socket.on('joinServer', (serverName) => {
-    const server = servers.find(s => s.name === serverName);
-    if (server) {
-      socket.emit('channelList', server.channels);
+  socket.on('create_channel', ({ server, channel }) => {
+    const srv = servers.find(s => s.name === server);
+    if (srv && channel.length > 0) {
+      srv.channels.push({ name: channel });
+      io.emit('channel_list', srv.channels);
     }
   });
 
-  // Create channel
-  socket.on('createChannel', ({ server, channel }) => {
-    const foundServer = servers.find(s => s.name === server);
-    if (foundServer && !foundServer.channels.find(c => c.name === channel)) {
-      foundServer.channels.push({ name: channel, messages: [] });
-      io.emit('channelList', foundServer.channels);
-      console.log(`Channel created: ${channel} in server ${server}`);
+  socket.on('get_channels', serverName => {
+    const srv = servers.find(s => s.name === serverName);
+    if (srv) {
+      io.to(socket.id).emit('channel_list', srv.channels);
     }
   });
 
-  // Join channel
-  socket.on('joinChannel', ({ server, channel }) => {
-    const foundServer = servers.find(s => s.name === server);
-    if (foundServer) {
-      const foundChannel = foundServer.channels.find(c => c.name === channel);
-      if (foundChannel) {
-        // Send previous messages
-        foundChannel.messages.forEach((msg) => {
-          socket.emit('message', msg);
-        });
+  socket.on('join_channel', ({ server, channel }) => {
+    socket.join(server + '-' + channel);
+  });
+
+  socket.on('chat_message', ({ server, channel, username, profilePic, message }) => {
+    const now = Date.now();
+    if (timeouts[username] && timeouts[username] > now) {
+      return; // User is timed out
+    }
+    io.to(server + '-' + channel).emit('chat_message', { username, profilePic, message });
+  });
+
+  // ADMIN EVENTS
+  socket.on('admin_strobe', () => {
+    if (users[socket.id]?.username === 'X12') {
+      io.emit('admin_strobe');
+    }
+  });
+
+  socket.on('admin_announcement', msg => {
+    if (users[socket.id]?.username === 'X12') {
+      io.emit('admin_announcement', msg);
+    }
+  });
+
+  socket.on('admin_kick', userToKick => {
+    if (users[socket.id]?.username === 'X12') {
+      const socketId = Object.keys(users).find(id => users[id].username === userToKick);
+      if (socketId) {
+        io.to(socketId).emit('kick');
+        io.sockets.sockets.get(socketId)?.disconnect();
       }
     }
   });
 
-  // Chat message
-  socket.on('chatMessage', ({ server, channel, content }) => {
-    const foundServer = servers.find(s => s.name === server);
-    if (foundServer) {
-      const foundChannel = foundServer.channels.find(c => c.name === channel);
-      if (foundChannel) {
-        const msg = {
-          username: socket.username || "Anonymous",
-          content: content
-        };
-        foundChannel.messages.push(msg);
-        io.emit('message', msg);
+  socket.on('admin_timeout', ({ user, time }) => {
+    if (users[socket.id]?.username === 'X12') {
+      const targetSocketId = Object.keys(users).find(id => users[id].username === user);
+      if (targetSocketId) {
+        timeouts[user] = Date.now() + time * 1000;
+        io.to(targetSocketId).emit('timeout', time);
       }
+    }
+  });
+
+  socket.on('admin_media', ({ imageUrl, audioUrl }) => {
+    if (users[socket.id]?.username === 'X12') {
+      io.emit('admin_media', { imageUrl, audioUrl });
     }
   });
 
   socket.on('disconnect', () => {
-    console.log('A user disconnected.');
+    console.log('User disconnected:', socket.id);
+    delete users[socket.id];
   });
 });
 
-// Start the server
+// Start server
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Sharcord server running on port ${PORT}`);
 });
