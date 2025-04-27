c65d1b3bf3e98569c144a18848f0fc60c537294f
x12fr <richrobux45@gmail.com>
2025-04-26 21:38:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 
 // Serve static files
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Handle page routes
+// Fake simple database (you can make this better later)
+let users = [];
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/chat', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'chat.html'));
 });
 
 app.get('/login', (req, res) => {
@@ -26,7 +26,38 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'register.html'));
 });
 
-// Handle socket events
+app.get('/chat', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'chat.html'));
+});
+
+// Handle login
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        res.redirect('/chat');
+    } else {
+        res.send('Login failed. <a href="/login">Try again</a>');
+    }
+});
+
+// Handle register
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    // Check if username is taken
+    const userExists = users.some(u => u.username === username);
+
+    if (userExists) {
+        res.send('Username already taken. <a href="/register">Try again</a>');
+    } else {
+        users.push({ username, password });
+        res.redirect('/login');
+    }
+});
+
+// Socket.io stuff
 io.on('connection', (socket) => {
     console.log('A user connected.');
 
@@ -56,13 +87,6 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         console.log('A user disconnected.');
-    });
-});
-
-// Custom kick handler (handle on client-side)
-io.on('connection', (socket) => {
-    socket.on('kicked user', (usernameToKick) => {
-        socket.username = usernameToKick;
     });
 });
 
