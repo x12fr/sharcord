47fbe0f759b6adcf632d4fcbbae87f3938a210a9
x12fr <richrobux45@gmail.com>
2025-04-26 21:05:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,102 +1,44 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const users = {};
+// Middleware
+app.use(express.static('public')); // serve static files from 'public' folder
+app.use(express.urlencoded({ extended: true })); // to read form data
 
-app.use(express.static(__dirname + '/public'));
-
-io.on('connection', (socket) => {
-  socket.on('registerUser', (username) => {
-    users[socket.id] = { username, pfp: '', timeout: false, tags: '' };
-    io.emit('userList', Object.values(users));
-  });
-
-  socket.on('chatMessage', ({ text, to }) => {
-    if (!users[socket.id].timeout) {
-      const data = {
-        username: users[socket.id].username,
-        message: text,
-        tags: users[socket.id].tags
-      };
-      if (to) {
-        const target = Object.keys(users).find(id => users[id].username === to);
-        if (target) io.to(target).emit('chatMessage', data);
-      } else {
-        io.emit('chatMessage', data);
-      }
-    }
-  });
-
-  socket.on('image', (img) => {
-    io.emit('image', {
-      username: users[socket.id].username,
-      img
-    });
-  });
-
-  socket.on('changePFP', (pfp) => {
-    users[socket.id].pfp = pfp;
-  });
-
-  socket.on('adminChangePFP', ({ user, pfp }) => {
-    const target = Object.keys(users).find(id => users[id].username === user);
-    if (target) {
-      users[target].pfp = pfp;
-    }
-  });
-
-  socket.on('adminRedirect', ({ user, url }) => {
-    const target = Object.keys(users).find(id => users[id].username === user);
-    if (target) {
-      io.to(target).emit('redirect', url);
-    }
-  });
-
-  socket.on('adminKick', (user) => {
-    const target = Object.keys(users).find(id => users[id].username === user);
-    if (target) {
-      io.to(target).disconnect();
-    }
-  });
-
-  socket.on('adminTimeout', ({ user, time }) => {
-    const target = Object.keys(users).find(id => users[id].username === user);
-    if (target) {
-      users[target].timeout = true;
-      users[target].tags = '[TIMEOUT]';
-      setTimeout(() => {
-        if (users[target]) {
-          users[target].timeout = false;
-          users[target].tags = '';
-        }
-      }, time * 1000);
-    }
-  });
-
-  socket.on('adminPlayAudio', (url) => {
-    io.emit('playAudio', url);
-  });
-
-  socket.on('adminStrobe', ({ duration }) => {
-    socket.emit('strobe', duration);
-  });
-
-  socket.on('adminStrobeAll', () => {
-    io.emit('strobe', 5);
-  });
-
-  socket.on('adminJumpscare', ({ img, audio }) => {
-    io.emit('jumpscare', { img, audio });
-  });
-
-  socket.on('disconnect', () => {
-    delete users[socket.id];
-    io.emit('userList', Object.values(users));
-  });
+// Pages
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-http.listen(3000, () => {
-  console.log('Sharcord server is up!');
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
+app.get('/chat', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'chat.html'));
+});
+
+// Form POST handlers
+app.post('/register', (req, res) => {
+  // TODO: Save new user logic (for now just redirect)
+  res.redirect('/login');
+});
+
+app.post('/login', (req, res) => {
+  // TODO: Login validation (for now just redirect)
+  res.redirect('/chat');
+});
+
+// Your socket.io logic goes here...
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
