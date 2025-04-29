a5b355631f56a6baa15d3f02e2f7a2b58a3be8a5
x12fr <richrobux45@gmail.com>
2025-04-29 19:06:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
-app.use(express.static(__dirname));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 let messages = [];
 
 io.on('connection', (socket) => {
+  // Send existing messages to the newly connected client
   messages.slice(-100).forEach(msg => socket.emit("message", msg));
 
+  // Handle incoming messages
   socket.on("message", data => {
     const msg = { ...data };
     messages.push(msg);
@@ -17,15 +26,19 @@ io.on('connection', (socket) => {
     io.emit("message", msg);
   });
 
+  // Handle background change requests
   socket.on("setBackground", (url) => {
     io.emit("changeBackground", url);
   });
 
+  // Handle sound play requests
   socket.on("playSound", (url) => {
     io.emit("playSound", url);
   });
 });
 
-http.listen(3000, () => {
-  console.log('Sharcord running on http://localhost:3000');
+// Start the server
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Sharcord running on http://localhost:${PORT}`);
 });
