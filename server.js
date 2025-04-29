f34a8083815988efac0b6a6bbe56dfbc4aaf710f
x12fr <richrobux45@gmail.com>
2025-04-29 16:56:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(http);
 
-const messages = []; // store up to 100 messages
+const messages = [];
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-io.on('connection', socket => {
-  console.log('A user connected');
-
-  // Send existing messages
+io.on('connection', (socket) => {
   socket.emit('init', messages);
 
-  // Receive and broadcast new messages
-  socket.on('chat message', data => {
+  socket.on('chat message', (data) => {
     const msg = {
       user: data.user,
       text: data.text,
-      isAdmin: data.isAdmin
+      isAdmin: data.isAdmin,
+      profilePic: data.profilePic
     };
-
     messages.push(msg);
     if (messages.length > 100) messages.shift();
-
     io.emit('chat message', msg);
+  });
+
+  socket.on('image message', (data) => {
+    const msg = {
+      user: data.user,
+      text: data.text,
+      isAdmin: data.isAdmin,
+      profilePic: data.profilePic
+    };
+    messages.push(msg);
+    if (messages.length > 100) messages.shift();
+    io.emit('image message', msg);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
