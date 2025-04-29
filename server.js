636b767256c3b05ab315051870121b1c5a6d216b
x12fr <richrobux45@gmail.com>
2025-04-29 16:45:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,76 +1,38 @@
 const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require('http');
+const { Server } = require('socket.io');
 const path = require('path');
-const fs = require('fs');
 
-const PORT = process.env.PORT || 3000;
-const ADMIN_KEY = 'admin123'; // Your admin key
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-let chatHistory = []; // Saved chat messages
+const messages = []; // store up to 100 messages
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+io.on('connection', socket => {
+  console.log('A user connected');
+
+  // Send existing messages
+  socket.emit('init', messages);
+
+  // Receive and broadcast new messages
+  socket.on('chat message', data => {
+    const msg = {
+      user: data.user,
+      text: data.text,
+      isAdmin: data.isAdmin
+    };
+
+    messages.push(msg);
+    if (messages.length > 100) messages.shift();
+
+    io.emit('chat message', msg);
+  });
 });
 
-io.on('connection', (socket) => {
-    console.log('A user connected.');
-
-    let userData = {};
-
-    socket.on('join', (data) => {
-        const isAdmin = data.adminKey === ADMIN_KEY;
-        userData = {
-            username: data.username,
-            pfp: data.pfp,
-            isAdmin: isAdmin
-        };
-
-        socket.emit('init', { isAdmin });
-
-        // Send chat history to the new user
-        chatHistory.forEach(msg => {
-            if (msg.type === 'text') {
-                socket.emit('chat message', msg.data);
-            } else if (msg.type === 'image') {
-                socket.emit('chat image', msg.data);
-            }
-        });
-    });
-
-    socket.on('chat message', (msg) => {
-        const messageData = {
-            username: userData.username,
-            message: msg,
-            pfp: userData.pfp,
-            isAdmin: userData.isAdmin
-        };
-
-        io.emit('chat message', messageData);
-        chatHistory.push({ type: 'text', data: messageData });
-    });
-
-    socket.on('chat image', (imageUrl) => {
-        const imageData = {
-            username: userData.username,
-            imageUrl: imageUrl,
-            pfp: userData.pfp,
-            isAdmin: userData.isAdmin
-        };
-
-        io.emit('chat image', imageData);
-        chatHistory.push({ type: 'image', data: imageData });
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected.');
-    });
-});
-
-http.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
