69cdf22265585d01d39a29f571bc96ad0efc2998
x12fr <richrobux45@gmail.com>
2025-04-29 02:09:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,63 +1,94 @@
-const express = require("express");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-const fs = require("fs");
-const path = require("path");
+const server = http.createServer(app);
+const io = new Server(server);
+
+const users = {};
+const timeouts = {};
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('register', ({ username, password, profilePic }) => {
+    if (Object.values(users).find(u => u.username === username)) {
+      socket.emit('registerFail', 'Username taken');
+    } else {
+      users[socket.id] = { username, password, profilePic, socket };
+      socket.emit('registerSuccess', { username, profilePic });
+      io.emit('userJoined', { username, profilePic });
+    }
+  });
+
+  socket.on('login', ({ username, password }) => {
+    const user = Object.values(users).find(u => u.username === username && u.password === password);
+    if (user) {
+      users[socket.id] = { ...user, socket };
+      socket.emit('loginSuccess', { username: user.username, profilePic: user.profilePic });
+    } else {
+      socket.emit('loginFail', 'Invalid credentials');
+    }
+  });
+
+  socket.on('chatMessage', (data) => {
+    const user = users[socket.id];
+    if (!user || timeouts[user.username]) return;
+
+    if (data.type === 'text') {
+      io.emit('chatMessage', {
+        username: user.username,
+        profilePic: user.profilePic,
+        message: data.message,
+        type: 'text'
+      });
+    } else if (data.type === 'image') {
+      io.emit('chatMessage', {
+        username: user.username,
+        profilePic: user.profilePic,
+        imageUrl: data.imageUrl,
+        type: 'image'
+      });
+    }
+  });
+
+  socket.on('adminCommand', (cmd) => {
+    const user = users[socket.id];
+    if (!user || user.username !== 'X12') return;
+
+    if (cmd.action === 'strobe') {
+      io.emit('strobeScreen', { duration: cmd.duration });
+    } else if (cmd.action === 'playAudio') {
+      io.emit('playAudio', { url: cmd.url });
+    } else if (cmd.action === 'timeout') {
+      const { target, duration } = cmd;
+      timeouts[target] = Date.now() + duration * 1000;
+      io.emit('timeoutApplied', { target, duration });
+      setTimeout(() => {
+        delete timeouts[target];
+        io.emit('timeoutLifted', { target });
+      }, duration * 1000);
+    } else if (cmd.action === 'redirect') {
+      io.to(cmd.targetSocketId).emit('redirect', cmd.url);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    const user = users[socket.id];
+    if (user) {
+      io.emit('userLeft', user.username);
+      delete users[socket.id];
+    }
+  });
+});
 
 const PORT = process.env.PORT || 3000;
-const messagesFile = "messages.json";
-
-// Serve static files (HTML, CSS, JS, etc.)
-app.use(express.static(path.join(__dirname)));
-
-// Serve the main page
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Load saved messages
-let messages = [];
-if (fs.existsSync(messagesFile)) {
-  try {
-    const data = fs.readFileSync(messagesFile);
-    messages = JSON.parse(data);
-  } catch (err) {
-    console.error("Failed to load messages:", err);
-  }
-}
-
-// Socket.IO connection
-io.on("connection", (socket) => {
-  console.log("A user connected");
-
-  // Send saved messages to new user
-  socket.emit("load messages", messages);
-
-  // New chat message
-  socket.on("chat message", (msg) => {
-    messages.push(msg);
-
-    // Save messages
-    fs.writeFile(messagesFile, JSON.stringify(messages), (err) => {
-      if (err) console.error("Failed to save messages:", err);
-    });
-
-    io.emit("chat message", msg); // Broadcast to everyone
-  });
-
-  // Image messages
-  socket.on("chat image", (imgData) => {
-    messages.push(imgData);
-
-    fs.writeFile(messagesFile, JSON.stringify(messages), (err) => {
-      if (err) console.error("Failed to save image messages:", err);
-    });
-
-    io.emit("chat image", imgData);
-  });
-});
-
-http.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+server.listen(PORT, () => {
+  console.log(`Sharcord server running on port ${PORT}`);
 });
