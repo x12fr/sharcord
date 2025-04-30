8759f42a9927c67a0e7452509ffda579c9c3f128
x12fr <richrobux45@gmail.com>
2025-04-29 22:32:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,73 @@
-const express = require("express");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-const path = require("path");
+const server = http.createServer(app);
+const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
-const adminKey = "331256444";
 
 let messages = [];
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+io.on('connection', socket => {
+  console.log('A user connected');
 
-io.on("connection", (socket) => {
-  socket.on("join", ({ username, isAdmin }) => {
+  socket.on('join', ({ username, isAdmin }) => {
     socket.username = username;
     socket.isAdmin = isAdmin;
-    socket.emit("loadMessages", messages);
+    console.log(`${username} joined. Admin: ${isAdmin}`);
+
+    // Send message history
+    socket.emit('loadMessages', messages);
   });
 
-  socket.on("sendMessage", (data) => {
-    const msg = {
+  socket.on('sendMessage', data => {
+    const message = {
       username: data.username,
       text: data.text,
       profilePicture: data.profilePicture,
       isAdmin: data.isAdmin,
-      type: "text"
+      type: 'text'
     };
-    messages.push(msg);
-    if (messages.length > 100) messages.shift();
-    io.emit("message", msg);
+
+    messages.push(message);
+    io.emit('message', message);
   });
 
-  socket.on("sendImage", (data) => {
-    const img = {
+  socket.on('sendImage', data => {
+    const message = {
       username: data.username,
       image: data.image,
       profilePicture: data.profilePicture,
       isAdmin: data.isAdmin,
-      type: "image"
+      type: 'image'
     };
-    messages.push(img);
-    if (messages.length > 100) messages.shift();
-    io.emit("message", img);
+
+    messages.push(message);
+    io.emit('message', message);
   });
 
-  socket.on("playAudio", (url) => {
-    io.emit("playAudio", url);
+  socket.on('changeBackground', url => {
+    if (socket.isAdmin) {
+      io.emit('changeBackground', url);
+    }
   });
 
-  socket.on("changeBackground", (url) => {
-    io.emit("changeBackground", url);
+  socket.on('playAudio', url => {
+    if (socket.isAdmin) {
+      io.emit('playAudio', url);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`${socket.username} disconnected`);
   });
 });
 
-http.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Sharcord server running on http://localhost:${PORT}`);
 });
